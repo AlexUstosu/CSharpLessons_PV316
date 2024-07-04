1f7ff806c411207565d3022f762c6c64219c6e7c
AlexUstosu <44317065+AlexUstosu@users.noreply.github.com>
2024-07-04 21:30:27 +0300

    Lessons file

---
+++ EmployeeClass_04.07.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+
+namespace ConsoleApp1
+{
+    class Employee
+    {
+        public string FIO { get; set; }
+        public double Salary { get; set; }
+
+        private DateTime startYear;
+        public DateTime StartYear 
+        {
+            get { return startYear; } 
+            set
+            {
+                if (value > DateTime.Now)
+                    startYear = DateTime.Now;
+                else
+                    startYear = value;
+            }
+        }
+        public Employee()
+        {
+            FIO = "Иванов Иван Иванович";
+            Salary = 128000.56D;
+            StartYear = new DateTime(2000, 12, 3);
+        }
+        public Employee(string fio, double salary, DateTime date)
+        {
+            FIO = fio;
+            Salary = salary;
+            StartYear = date;
+        }
+        public int CurrentSeniority()
+        {
+            return DateTime.Now.Year - StartYear.Year;
+        }
+        public int CountDaysOfSeniotity()
+        {
+            int years = CurrentSeniority();
+            return years * 365;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"Employee: {FIO} - Salary: {Salary.ToString()}\t" +
+                             $"StartYear: {StartYear.Year.ToString()}");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee[] employees =
+            {
+                new Employee("Емильян Федорович Петров",25369.85,new DateTime(2001,3,25)),
+                new Employee("Светлана Владимировна Агапова",127333.01,new DateTime(2023,5,25)),
+                new Employee("Юлия Борисовна Михеева",35600,new DateTime(2019,3,25)),
+                new Employee("Андрей Вячеславович Богатько",78001.20,new DateTime(1990,3,25))
+            };
+
+            foreach (var employee in employees)
+            {
+                employee.Show();
+                Console.WriteLine($"Стаж: {employee.CurrentSeniority()}\t Дней: {employee.CountDaysOfSeniotity()}");
+
+            }
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Program_04.07.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+
+        public Point()
+        {
+            this.X = 0;
+            this.Y = 0;
+            this.Z = 0;
+
+        }
+        public Point(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+        public string Show()
+        {
+            return $"X = {this.X}, Y = {this.Y}, Z = {this.Z}\t";
+        }
+    }
+    class Triangle
+    {
+        public Point Point1 { get; set; }
+        public Point Point2 { get; set; }
+        public Point Point3 { get; set; }
+
+        public Triangle()
+        {
+            Point1 = new Point(0, 0, 0);
+            Point2 = new Point(1, 2, 3);
+            Point3 = new Point(5, 1, 6);
+        }
+        public void Show()
+        {
+            Console.WriteLine($"Triangle: {Point1.Show()},  {Point2.Show()}, {Point3.Show()}");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle triangle = new Triangle();
+            triangle.Show();
+
+            triangle.Point1.Y = 15;
+            triangle.Point1.Z = 100;
+
+
+            triangle.Point2 = new Point(2, 8, 9);
+
+            Point newPoint = triangle.Point3;
+
+            Console.WriteLine(newPoint.Show());
+            triangle.Show();
+
+            Point p1 = null;
+            int? new_x = p1?.X;     //nullable type int переменной
+                                    //new_x при условии, что p1 не равно NULL
+                                    //возмет значение свойства X
+
+            Point p2 = null;                    //стандартный механизм проверки 
+            if (p2 == null)                     //переменной reference-type 
+                p2 = new Point(0, 0, 0);        //на NULL 
+
+            Point p3 = null;
+            p3 = p3 ?? new Point(0, 0, 0);      //Упрощенная модель проверки 
+                                                //переменной reference-type 
+                                                //на NULL
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
+
